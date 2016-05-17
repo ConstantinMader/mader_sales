6724833e0edbf1c9b8720e1866eda6dee5c9ed8d
Constantin <mad17204@spengergasse.at>
2016-05-17 20:21:03 +0200

    Customers

---
+++ Mader_sales/src/model/Customers.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Customers {
+	private  ArrayList<Customer> customers;
+	public Customers() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
